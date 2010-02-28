b7664d987a529c408695c3cc74764286e19e9d48
mbern <mbern>
2010-02-28 09:23:40 +0000

    Erweiterungen fuer Shell

--- SRCPCore/src/lan/EthernetSRCPServer.h
+++ SRCPCore/src/lan/EthernetSRCPServer.h
@@ -64,6 +64,8 @@ public:
 	void addDeviceManager( srcp::SRCPDeviceManager* manager ) { devices->addDeviceManager( manager ); }
 	void begin( byte mac[], byte ip[], int port, srcp::device_config_t deviceConfig[], int id, int version );
 	void run();
+	srcp::SRCPDeviceMaster* getDevices() { return ( devices ); }
+	srcp::SRCPSession* getSession() { return( srcpSession ); }
 
 	virtual ~EthernetSRCPServer();
 };
