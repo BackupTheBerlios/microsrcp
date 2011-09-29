4cbf9ba41064c1bceaaa2ad19770dc781ff02899
mbern <mbern>
2011-09-29 12:32:07 +0000

    Layout fuer IO Shield von DF Robot eingetragen (fuer Arduino und Mega)

--- SRCPGASlave/src/SRCPGASlave.cpp
+++ SRCPGASlave/src/SRCPGASlave.cpp
@@ -29,7 +29,7 @@
 #include "dev/CoreDeviceManager.h"
 #include "i2c/I2CServer.h"
 
-#define VERSION 13	// Version x.y der EEPROM Daten
+#define VERSION 11	// Version x.y der EEPROM Daten
 
 /*// Definition der lokalen Geraete - 1. Version, als Arduino Shield aufgebaut.
 srcp::device_config_t deviceConfig[] =
@@ -52,7 +52,7 @@ srcp::device_config_t deviceConfig[] =
 
 // Definition der lokalen Geraete - 2. Version, minimale Bausteine (Resonator, 10K, Atmega328P)
 // aufgebaut auf Laborkarte RE315 von Roth Elektronik. Kein Arduino Standard Board mehr!
-srcp::device_config_t deviceConfig[] =
+/*srcp::device_config_t deviceConfig[] =
 	{
 		// lokale Adresse 1, Servo an Pin 0, Servoradius von 87 - 93 (Mittelstellung) verwenden.
 		{ 1, 0, srcp::GA, srcp::GAServo , {9, 87, 93} },	// Servo Pin D9 und D0 - D4
@@ -68,7 +68,86 @@ srcp::device_config_t deviceConfig[] =
 		{ 1, 8, srcp::FB, srcp::FBSwitchSensor, { 10, 17 } },	// Sensoren Pin D10 - D13 und C0 - C3
 		// EOF Geraete - nicht vergessen!
 		{ -1 },
+};*/
+
+// Definition der lokalen Geraete - 3. Version, mit DFRobot IO Expansion Shield For Arduino(V5)
+srcp::device_config_t deviceConfig[] =
+	{
+		// lokale Adresse 7, Lichtsignal
+		{ 7, 0, srcp::GA, srcp::GASignal, {4, 5} },		// Signale Pin D5 + D6 und D7 + D8
+		{ 8, 0, srcp::GA, srcp::GASignal, {6, 7} },
+		// lokale Adresse 1, Servo an Pin 0, Servoradius von 87 - 93 (Mittelstellung) verwenden.
+		{ 1, 0, srcp::GA, srcp::GAServo , {8, 87, 93} },	// Servo Pin 0 - 5
+		{ 2, 0, srcp::GA, srcp::GAServo , {9, 87, 93} },
+		{ 3, 0, srcp::GA, srcp::GAServo , {10, 87, 93} },
+		{ 4, 0, srcp::GA, srcp::GAServo , {11, 87, 93} },
+		{ 5, 0, srcp::GA, srcp::GAServo , {12, 87, 93} },
+		{ 6, 0, srcp::GA, srcp::GAServo , {13, 87, 93} },
+
+		// lokale Adresse 1 bis 8, Rueckmelder an Pins 14 - 17
+		//{ 1, 8, srcp::FB, srcp::FBSwitchSensor, { 0, 3 } },		// Digital Pin 0 - 3
+		{ 1, 8, srcp::FB, srcp::FBSwitchSensor, { 14, 17 } },	// Sensoren Pin ADC 0 - 4
+		// EOF Geraete - nicht vergessen!
+		{ -1 },
 };
+// Definition der lokalen Geraete - 4. Version, mit DFRobot IO Expansion Shield For Arduino Mega!
+/*srcp::device_config_t deviceConfig[] =
+	{
+		// Pin 0 + 1 Serielle Schnittstelle 1 - frei
+		// lokale Adresse 1, Servos an Pin 2 - 37 Servoradius von 87 - 93 (Mittelstellung) verwenden.
+		{  1, 0, srcp::GA, srcp::GAServo , {2, 87, 93} },
+		{  2, 0, srcp::GA, srcp::GAServo , {3, 87, 93} },
+		{  3, 0, srcp::GA, srcp::GAServo , {4, 87, 93} },
+		{  4, 0, srcp::GA, srcp::GAServo , {5, 87, 93} },
+		{  5, 0, srcp::GA, srcp::GAServo , {6, 87, 93} },
+		{  6, 0, srcp::GA, srcp::GAServo , {7, 87, 93} },
+		{  7, 0, srcp::GA, srcp::GAServo , {8, 87, 93} },
+		{  8, 0, srcp::GA, srcp::GAServo , {9, 87, 93} },
+		{  9, 0, srcp::GA, srcp::GAServo , {10, 87, 93} },
+		{ 10, 0, srcp::GA, srcp::GAServo , {11, 87, 93} },
+		{ 11, 0, srcp::GA, srcp::GAServo , {12, 87, 93} },
+		{ 12, 0, srcp::GA, srcp::GAServo , {13, 87, 93} },
+		{ 13, 0, srcp::GA, srcp::GAServo , {14, 87, 93} },
+		{ 14, 0, srcp::GA, srcp::GAServo , {15, 87, 93} },
+		{ 15, 0, srcp::GA, srcp::GAServo , {16, 87, 93} },
+		{ 16, 0, srcp::GA, srcp::GAServo , {17, 87, 93} },
+		{ 17, 0, srcp::GA, srcp::GAServo , {18, 87, 93} },
+		{ 18, 0, srcp::GA, srcp::GAServo , {19, 87, 93} },
+                // Pin 20 + 21 I2C
+		{ 19, 0, srcp::GA, srcp::GAServo , {22, 87, 93} },
+		{ 20, 0, srcp::GA, srcp::GAServo , {23, 87, 93} },
+		{ 21, 0, srcp::GA, srcp::GAServo , {24, 87, 93} },
+		{ 22, 0, srcp::GA, srcp::GAServo , {25, 87, 93} },
+		{ 23, 0, srcp::GA, srcp::GAServo , {26, 87, 93} },
+		{ 24, 0, srcp::GA, srcp::GAServo , {27, 87, 93} },
+		{ 25, 0, srcp::GA, srcp::GAServo , {28, 87, 93} },
+		{ 26, 0, srcp::GA, srcp::GAServo , {29, 87, 93} },
+		{ 27, 0, srcp::GA, srcp::GAServo , {30, 87, 93} },
+		{ 28, 0, srcp::GA, srcp::GAServo , {31, 87, 93} },
+		{ 29, 0, srcp::GA, srcp::GAServo , {32, 87, 93} },
+		{ 30, 0, srcp::GA, srcp::GAServo , {33, 87, 93} },
+		{ 31, 0, srcp::GA, srcp::GAServo , {34, 87, 93} },
+		{ 32, 0, srcp::GA, srcp::GAServo , {35, 87, 93} },
+		{ 33, 0, srcp::GA, srcp::GAServo , {36, 87, 93} },
+		{ 34, 0, srcp::GA, srcp::GAServo , {37, 87, 93} },
+
+		// Signale Pin 38 - 53
+		{ 35, 0, srcp::GA, srcp::GASignal, {38, 39} },
+		{ 36, 0, srcp::GA, srcp::GASignal, {40, 41} },
+		{ 37, 0, srcp::GA, srcp::GASignal, {42, 43} },
+		{ 38, 0, srcp::GA, srcp::GASignal, {44, 45} },
+		{ 39, 0, srcp::GA, srcp::GASignal, {46, 47} },
+		{ 40, 0, srcp::GA, srcp::GASignal, {48, 49} },
+                // Pin 50 - 53 doppelt belegt - SPI Schnittstelle
+		{ 41, 0, srcp::GA, srcp::GASignal, {50, 51} },
+		{ 42, 0, srcp::GA, srcp::GASignal, {52, 53} },
+
+		// lokale Adresse 1 bis 16, Rueckmelder an Pins A0 - A15
+		{ 1, 8, srcp::FB, srcp::FBSwitchSensor, { 54, 61 } },
+		{ 9, 16, srcp::FB, srcp::FBSwitchSensor, { 62, 69 } },
+		// EOF Geraete - nicht vergessen!
+		{ -1 },
+};*/
 
 void setup()
 {
@@ -77,7 +156,7 @@ void setup()
 #endif
 
 	WireServer.addDeviceManager( new dev::CoreDeviceManager() );
-	WireServer.begin( deviceConfig, srcp::BOARD_GA, VERSION );
+	WireServer.begin( deviceConfig, srcp::BOARD_GA, VERSION, 11 );
 
 #if	( DEBUG_SCOPE > 0 )
 	Serial << "GA listen " <<  WireServer.getMyAddr() << endl;
